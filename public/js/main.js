1b33f337b9ad84977d2e7265ce73a503dfe898c9
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2024-12-13 02:15:03 +0700

    feat: main content and footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,24 @@
-let navbarToggles = document.querySelectorAll('.navbar-toggle .toggle-button');
-let sidebar = document.querySelector('.sidebar');
+const navbarToggles = document.querySelectorAll('.navbar-toggle .toggle-button');
+const sidebar = document.querySelector('.sidebar');
 
 navbarToggles.forEach((navbarToggle) => {
   navbarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('show');
+  });
+});
+
+const counters = document.querySelectorAll('#statistic .value');
+
+counters.forEach((counter) => {
+  let dataCounter = Number(counter.dataset.counter);
+  let number = 0;
+  counter.addEventListener('click', () => {
+    while (number < dataCounter) {
+      setTimeout(() => {
+        number++
+      }, 1000);
+    }
+
+    counter.textContent = number;
   })
 });
