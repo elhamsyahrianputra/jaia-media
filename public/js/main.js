0996da255c7f078e4b0a2f32674a33b0db66838a
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2024-12-14 15:24:50 +0700

    feat: effect and responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,74 @@
 const navbarToggles = document.querySelectorAll('.navbar-toggle .toggle-button');
 const sidebar = document.querySelector('.sidebar');
+const body = document.querySelector('body');
 
-navbarToggles.forEach((navbarToggle) => {
-  navbarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', (event) => {
+    const isClickInsideSidebar = sidebar.contains(event.target);
+    const isClickOnToggleBtn = document.querySelector('.navbar-toggle .toggle-button').contains(event.target);
+
+    if (!isClickInsideSidebar && !isClickOnToggleBtn && sidebar.classList.contains('show')) {
+      sidebar.classList.remove('show');
+      body.classList.remove('overflow-hidden');
+    }
   });
+
+  navbarToggles.forEach((navbarToggle) => {
+    navbarToggle.addEventListener('click', () => {
+      sidebar.classList.toggle('show');
+      body.classList.toggle('overflow-hidden');
+
+    });
+  });
+})
+
+
+
+
+
+// Counter Animation
+function counterAnimate(target) {
+  let data = Number(target.dataset.counter);
+  let startValue = 0;
+  let currentValue = startValue;
+  const duration = 800;
+
+  let increment = data / duration * 25;
+
+  const incrementAnimation = setInterval(() => {
+    currentValue += increment;
+    target.textContent = Math.round(currentValue);
+    
+    if (currentValue > data) {
+      target.textContent = data;
+      clearInterval(incrementAnimation);
+    }
+
+  }, 25);
+}
+
+const plus = document.querySelectorAll('#statistic .plus');
+
+// Play counter when in viewport
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        counterAnimate(entry.target);
+        plus.forEach((item) => {
+          item.classList.add('solid');
+        });
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.75,
+  },
+);
+
+const counters = document.querySelectorAll('#statistic .value');
+
+counters.forEach((counter) => {
+  observer.observe(counter);
 });
-
-// const counters = document.querySelectorAll('#statistic .value');
-
-// counters.forEach((counter) => {
-//   let dataCounter = Number(counter.dataset.counter);
-//   let number = 0;
-//   counter.addEventListener('click', () => {
-//     while (number < dataCounter) {
-//       setTimeout(() => {
-//         number++
-//       }, 1000);
-//     }
-
-//     counter.textContent = number;
-//   })
-// });
